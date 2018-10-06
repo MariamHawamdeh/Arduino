494aa4d3ad6b6e9e2f2f43fbacf8b9519c9f0918
Mariam B. Hawamdeh <141032@ppu.edu.ps>
2018-10-06 17:07:42 +0300

    Update task_bluetooth.ino

--- task_bluetooth.ino
+++ task_bluetooth.ino
@@ -1,27 +1,27 @@
-const int G = 13;
-const int R = 12;
-const int c =5;
-char myPassword[] = { '1', '9', '9', '7','\0'};
-int cnt =0;
-char pass[c] = {'\0'};
+const int G = 13; // Green LED with pin 13
+const int R = 12; // Red LED with pin 12
+const int c =5; // 5 elements in the array
+char myPassword[] = { '1', '9', '9', '7','\0'}; // PIN 
+int cnt =0; // counter
+char pass[c] = {'\0'}; // array for the entered PIN
 void setup() {
 pinMode(G,OUTPUT);
 pinMode(R,OUTPUT);
-Serial.begin(9600);
+Serial.begin(9600); // bluetooth serial
 }
 
 void loop() {
 
 if(Serial.available())
 {
-  for (int i=0; i<c;i++)
+  for (int i=0; i<c;i++) // read the entered PIN 
  { pass[i] = Serial.read();
  }
-  for (int j=0; j<c;j++)
+  for (int j=0; j<c;j++) // compare the entered PIN with  myPassword[]
  {if(pass[j] == myPassword[j])
- cnt++;
+ cnt++; // if the digit correct increment to the counter 
  }
- if(cnt==c-1)
+ if(cnt==c-1)   // if the 4 digits entered are correct light up the green LED
  {digitalWrite(G,HIGH);
   digitalWrite(R,LOW);}
   else 
@@ -29,13 +29,14 @@ if(Serial.available())
   digitalWrite(G,LOW);
   }
 }
-else 
+else  
 {
   digitalWrite(G,LOW);
   digitalWrite(R,LOW); 
 }
-delay(1000);
-pass[c] = {'\0'};
-cnt =0;
+delay(1000); 
+pass[c] = {'\0'}; // clear the array 
+cnt =0; // reset the counter
+  
 }
  
