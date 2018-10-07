1ed1542b5307eb95e3389f5fe2a2a791dbfe346c
Mariam B. Hawamdeh <141032@ppu.edu.ps>
2018-10-07 21:04:45 +0300

    Update task_bluetooth.ino

--- task_bluetooth.ino
+++ task_bluetooth.ino
@@ -1,42 +1,60 @@
-const int G = 13; // Green LED with pin 13
-const int R = 12; // Red LED with pin 12
-const int c =5; // 5 elements in the array
-char myPassword[] = { '1', '9', '9', '7','\0'}; // PIN 
-int cnt =0; // counter
-char pass[c] = {'\0'}; // array for the entered PIN
+const int G = 13;
+const int R = 12;
+const int c =5;
+char myPassword[] = { '1', '9', '9', '7','\0'};
+int cnt =0;
+char pass[c] = {'\0'};
+const int buzzer = 10;
+int motorPin =  5;    
+    
+
 void setup() {
 pinMode(G,OUTPUT);
 pinMode(R,OUTPUT);
-Serial.begin(9600); // bluetooth serial
+pinMode(buzzer, OUTPUT);
+Serial.begin(9600);
 }
 
 void loop() {
 
 if(Serial.available())
 {
-  for (int i=0; i<c;i++) // read the entered PIN 
+  for (int i=0; i<c;i++)
  { pass[i] = Serial.read();
  }
-  for (int j=0; j<c;j++) // compare the entered PIN with  myPassword[]
+  for (int j=0; j<c;j++)
  {if(pass[j] == myPassword[j])
- cnt++; // if the digit correct increment to the counter 
+ cnt++;
  }
- if(cnt==c-1)   // if the 4 digits entered are correct light up the green LED
+ if(cnt==c-1)
  {digitalWrite(G,HIGH);
-  digitalWrite(R,LOW);}
+  digitalWrite(R,LOW);
+  analogWrite(motorPin, 90); //rotates motor
+   // set the Pin motorPin1 LOW
+  
+    // set the Pin motorPin2 LOW
+  tone(buzzer, 2000,100); // tone( pin number, frequency in hertz, duration in milliseconds);
+  delay(1000);        // ...for 1 sec
+  noTone(buzzer);
+  delay(4000); // Stop sound...
+  digitalWrite(motorPin, LOW); 
+  }
   else 
   { digitalWrite(R,HIGH);
   digitalWrite(G,LOW);
+  tone(buzzer, 1000,1000); // Send 1KHz sound signal...
+  delay(1000);        // ...for 1 sec
+  noTone(buzzer);     // Stop sound...
+       
   }
 }
-else  
+else 
 {
   digitalWrite(G,LOW);
   digitalWrite(R,LOW); 
 }
-delay(1000); 
-pass[c] = {'\0'}; // clear the array 
-cnt =0; // reset the counter
-  
+delay(1000);
+pass[c] = {'\0'};
+cnt =0;
 }
  
